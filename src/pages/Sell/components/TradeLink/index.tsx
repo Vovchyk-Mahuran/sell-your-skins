bc1aba59be768801326f486d817a329e8521444d
Vovchyk Mahuran <vovchukmah@gmail.com>
2023-04-02 12:10:58 +0300

    added trade url link

--- src/pages/Sell/components/TradeLink/index.tsx
+++ src/pages/Sell/components/TradeLink/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 // Hooks
 import { useTradeLink } from 'shared/hooks/useTradeLink';
@@ -11,6 +12,9 @@ const TradeLink: FC = () => {
 	const { url, changeUrl, updateTradeUrl, urlError } = useTradeLink();
 	return (
 		<div className="equipment__url">
+			<Link target="_blank" to="http://steamcommunity.com/my/tradeoffers/privacy#trade_offer_access_url">
+				Your <span className="marked">trade url:</span>
+			</Link>
 			<Input
 				value={url}
 				onChange={changeUrl}
