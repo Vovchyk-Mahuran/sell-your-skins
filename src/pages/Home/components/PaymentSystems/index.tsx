99a08ee517f2a7f6cab75d3b425f83ba566c2e39
Boykosoft <detroit324@gmail.com>
2023-04-02 09:50:30 +0100

    payment systems

--- src/pages/Home/components/PaymentSystems/index.tsx
+++ src/pages/Home/components/PaymentSystems/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
 
 // Constants
-import { PAYMENT_SYSTEMS } from 'constants/paymentSystems';
+//import { PAYMENT_SYSTEMS } from 'constants/paymentSystems';
+import { CHECKOUT_SYSTEMS } from 'constants/checkoutMethods';
 
 // Styles
 import './index.scss';
@@ -9,7 +10,7 @@ import './index.media.scss';
 
 const PaymentSystems: FC = () => (
 	<div className="payment-systems__container container">
-		{PAYMENT_SYSTEMS.map(system => (
+		{CHECKOUT_SYSTEMS.map(system => (
 			<img className="payment-system" key={system.title} src={system.src} alt={system.title} />
 		))}
 	</div>
