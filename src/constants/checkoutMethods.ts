94c9818c3ac6e9bc01d7510c66ea93db7ee7578e
Boykosoft <detroit324@gmail.com>
2023-04-15 17:02:34 +0100

    paypal

--- src/constants/checkoutMethods.ts
+++ src/constants/checkoutMethods.ts
@@ -6,7 +6,7 @@ import { PaymentSystem } from 'core/types/payment-system.type';
 
 // Images
 import Card from 'assets/icons/card.svg';
-import PayPal from 'assets/icons/paypal.svg.svg';
+import PayPal from 'assets/icons/paypal.svg';
 import Bitcoin from 'assets/icons/bitcoin.svg';
 import TetherTRC20 from 'assets/icons/tether-trc.svg';
 import TetherERC20 from 'assets/icons/tether-erc.svg';
