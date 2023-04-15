f71eaf9a392c0434946fbde531f24eac9d79ad19
Boykosoft <detroit324@gmail.com>
2023-04-15 19:11:16 +0100

    sell button i,proves

--- src/shared/hooks/usePaymentForm.ts
+++ src/shared/hooks/usePaymentForm.ts
@@ -42,8 +42,8 @@ export const usePaymentForm = (selected: string) => {
 	}, [currentMethod]);
 
 	const isButtonDisabled = useMemo(
-		() => !userTransactionValue || totalWithFee < 0 || !!urlError,
-		[userTransactionValue, totalWithFee, urlError]
+		() => !userTransactionValue || !currentMethod || totalWithFee < currentMethod.min + 0.01 || !!urlError,
+		[userTransactionValue, totalWithFee, urlError, currentMethod]
 	);
 
 	return { fee, totalPrice, amount, currentMethod, isButtonDisabled };
