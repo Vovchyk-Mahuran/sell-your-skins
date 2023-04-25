3c3d733037cc414c1c51764f138488e1a3bab073
Boykosoft <detroit324@gmail.com>
2023-04-25 17:08:32 +0100

    methods imgs

--- src/constants/checkoutMethods.ts
+++ src/constants/checkoutMethods.ts
@@ -9,10 +9,14 @@ import { PaymentSystem } from 'core/types/payment-system.type';
 // Images
 import Card from 'assets/icons/card.svg';
 import PayPal from 'assets/icons/paypal.svg';
-import Bitcoin from 'assets/icons/bitcoin.svg';
-import TetherTRC20 from 'assets/icons/tether-trc.svg';
-import TetherERC20 from 'assets/icons/tether-erc.svg';
-import Ethereum from 'assets/icons/ethereum.svg';
+import Bitcoin from 'assets/icons/skrill.svg';
+//import Bitcoin from 'assets/icons/bitcoin.svg';
+import TetherTRC20 from 'assets/icons/yoomoney.svg';
+// import TetherTRC20 from 'assets/icons/tether-trc.svg';
+import TetherERC20 from 'assets/icons/webmoney.svg';
+// import TetherERC20 from 'assets/icons/tether-erc.svg';
+import Ethereum from 'assets/icons/bank.svg';
+// import Ethereum from 'assets/icons/ethereum.svg';
 
 export const CHECKOUT_SYSTEMS: PaymentSystem[] = [
 	{
