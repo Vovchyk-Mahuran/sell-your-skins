1f4a75da91088c7ea72b289f1162f66eb18969e5
vitatov <vitatov44@gmail.com>
2023-04-25 11:02:22 +0300

    Update contuct us

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -5,11 +5,11 @@ const ContactUs: FC = () => {
 	return (
 		<section className="contact-us">
 			<Layout classes="container">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint explicabo asperiores commodi sit
-				quibusdam accusamus rerum officiis officia quam rem voluptas eos qui, dignissimos distinctio nihil in
-				molestiae facilis hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita
-				optio nobis, nihil hic molestiae pariatur debitis veniam tempora omnis ipsum quibusdam soluta vitae
-				dicta illo culpa officia perferendis ab?
+			If you have any questions, comments, or concerns about our website, please feel free to get in touch with us using any of the methods below. We appreciate your feedback and are committed to providing the best possible user experience for our visitors.
+
+				Email: <a href="mailto:support@skinsdeals.com">support@skinsdeals.com</a>
+
+			You can also reach out to us through our 24/7 online chat service, available on our website. Our customer support representatives are always available to assist you with any questions or issues you may have.
 			</Layout>
 		</section>
 	);
